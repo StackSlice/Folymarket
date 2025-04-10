199cd6f55c5d4cf9e3b276526a0d21db39a21091
StackSlice <actionarts11@gmail.com>
2025-04-09 21:29:40 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Folymarket from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Folymarket />
+  </React.StrictMode>
+);
